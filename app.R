4c5db0fd4f50af596dc22da631e00fa3253bb4f7
Michael Johannes Ohlinger <mohlinge@ad.uni-mannheim.de>
2022-03-04 19:27:49 +0100

    Ich habe das Skript (sehr) ausführlich kommentiert. Ich hoffe, es ist ok, dass ich auf englisch kommentiert habe. Das ist m.E. sinnvoller, da nahezu alle Ressourcen im Internet und auch die Befehle auf englisch sind.

--- app.R
+++ app.R
@@ -1,7 +1,53 @@
-library(shiny)
-library(shinysurveys)
-library(readxl)
+#1     Preparations####
 
+#1-1   Import libraries####
+
+#1-1-1 Define library function####
+
+#First define a function that loads a package into the library and installs it
+#beforehand if the package is not yet installed.
+
+#pck: The name of the desired package.
+libr <- function(pck){
+  if(!require(pck, character.only = T)){
+    install.packages(pck)
+    library(pck, character.only = T)
+  }
+}
+
+#1-1-2 Import libraries####
+
+#Load and install, if necessary, the packages needed.
+libr("shiny")        #Necessary for shiny itself.
+libr("shinysurveys") #Necessary for the survey part.
+libr("readxl")       #Necessary for reading the excel-File we want to append the results
+                     #of the survey to.
+libr("writexl")      #Will probably be necessary for writing the excel-File in the end.
+
+#1-2   Import data####
+
+#1-2-1 Survey data####
+
+#This right now is not very neat and ugly. the survey takes its input (questions etc.) 
+#from a data.frame. Here two data.frames are created manually, but I think it would be
+#easier and more beautiful to just read a .csv or similar containing the parameters as
+#variables, as proposed in Questions.xlsx.
+#question:         The question that appears in the survey.
+#option:           Not exactly sure what this does, in text-inputs it appears to be the 
+#                  text that is in the input field in the beginning.
+#input_type:       This seems to define if the question is numeric, text, or another type
+#                  (I guess that multiple-choice is possible). Not sure what is possible
+#                  here.
+#input_id          This is the name of the variable the answer to this question will be
+#                  saved. It can be obtained in the server-part using input$input_id.
+#dependence:       This is kind of a connection to other questions like "show this question if
+#                  another question was answered yes" but not sure how it works
+#dependence_value: This is also for the connection to other questions. Maybe dependence
+#                  defines which question and dependence_value which value is needed.
+#required:         This specifies if the question must be answered, not sure what happens
+#                  if the question is not answered. Maybe a warning.
+
+#Create a data.frame q1 with one question in it.
 q1 <- data.frame(question = "What is your favorite food?",
                  option = "Your Answer",
                  input_type = "text",
@@ -9,6 +55,8 @@ q1 <- data.frame(question = "What is your favorite food?",
                  dependence = NA,
                  dependence_value = NA,
                  required = F)
+
+#Create a data.frame q2 with another question in it.
 q2 <- data.frame(question = "What is your  food?",
                  option = "Your Answer",
                  input_type = "text",
@@ -16,20 +64,61 @@ q2 <- data.frame(question = "What is your  food?",
                  dependence = NA,
                  dependence_value = NA,
                  required = F)
+
+#Put both data.frames together into one data.frame df
 df <- rbind(q1, q2)
 
+#1-2-2 Table####
+
+#Here, I think, we should load the data from the server to append the answers to later.
+
+#2     Shiny-App####
+
+#Now the fun begins. The shiny-app consists out of two parts: the user-interface, where we
+#define how the things the user sees look like and behave
+
+#2-1   Page####
+
+#fluidPage generates the user-interface
 ui <- fluidPage(
+  
+    #surveyOutput creates a questionnaire.
+    #df:                 The data.frame containing the questions.
+    #survey_title:       Should be shown at the top of the page
+    #survey_description: Should be shown underneath the title but not sure.
     surveyOutput(df = df,
                  survey_title = "Hello, World!",
                  survey_description = "Welcome! This is a demo survey showing off the {shinysurveys} package."),
+    
+    #This generates an output which is specified as output$text in the server-part.
+    #In general that's how you create reactive output. Define it as output$xy in the
+    #server part and put an appropriate output function into the ui-part (there's e.g.,
+    #tableOutput, plotOutput, ...). Also note that in the ui-part the outputs are
+    #separated with commas.
     htmlOutput("text")
 )
 
+#2-2   Server####
+
+#I don't really know why server needs input, output, and session as parameters but it
+#seems to work like this.
 server <- function(input, output, session) {
+    
+    #I think this is necessary for the survey but I have no clue (yet) what it does.
     renderSurvey()
+  
+    #This takes the text written in the question with question_id favorite and puts it
+    #into output$text to display it in the user interface. This is just a proof of concept
+    #and to have something where one can see how this works and what is necessary a) to
+    #use input inside the ui and b) to access the input from the questionnaire to append
+    #to the data later.
     output$text <- renderText({ 
       input$favorite 
     })
+    
+    #This reacts to a press on the (I guess automatically with SurveyOutput generated)
+    #submit-button by displaying a dialogue window containing text. In general one can
+    #react to things happening with user input using observeEvent()
     observeEvent(input$submit, {
         showModal(modalDialog(
             title = "Congrats, you completed your first shinysurvey!",
@@ -38,4 +127,7 @@ server <- function(input, output, session) {
     })
 }
 
+#3     Start the app####
+
+#This starts the app using the previously defined ui and server.
 shinyApp(ui, server)
